04e7115ff2f3f679398921ce6315a7fcd7e8b8cd
Dariya Popova <dariyp26@mail.ru>
2022-08-28 19:02:34 +0300

    ДЗ к семинару 3

---
+++ task19/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+bool Palindrom(int num)
+{
+  int number = num;
+  int rev_number = 0;
+  while (number > 0)
+  {
+    int rev = number % 10;
+    rev_number = rev_number * 10 + rev;
+    number = number / 10;
+  }
+  return (num == rev_number);
+}
+bool x5(int num){
+  int i = 0;
+  while (Math.Abs(num) > 0)
+  {
+    i++;
+    num/=10;
+  }
+  return (i==5);
+}
+
+Console.WriteLine("Задайте целое пятизначное число: ");
+try
+{
+  int x = Convert.ToInt32(Console.ReadLine());
+  if (x5(x))
+  {
+    if (Palindrom(Math.Abs(x)))
+    {
+      Console.WriteLine("Это палиндромом.");
+    }
+    else
+    {
+      Console.WriteLine("Это не палиндромом.");
+    }
+  }
+  else
+  {
+    Console.WriteLine("Число не пятизначное.");
+  }
+
+}
+catch
+{ Console.WriteLine("Не корректный ввод данных."); }
---
+++ task21/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
+
+int[] A = {
+    new Random().Next(-9,10),
+    new Random().Next(-9,10), 
+    new Random().Next(-9,10)
+    };
+
+int[] B = {
+    new Random().Next(-9,10),
+    new Random().Next(-9,10), 
+    new Random().Next(-9,10)
+    };
+
+double distance = Math.Sqrt(
+  Math.Pow(B[0]-A[0],2)+
+  Math.Pow(B[1]-A[1],2)+
+  Math.Pow(B[2]-A[2],2)
+);
+Console.WriteLine("Точка А [" + A[0] + "," + A[1] + "," + A[2] + "].");
+Console.WriteLine("Точка B [" + B[0] + "," + B[1] + "," + B[2] + "].");
+Console.WriteLine("Расстояние между точками {0:0.00}.", distance);
